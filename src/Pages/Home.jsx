f18aec1ed9f3c754ab7d4f94bd346064738e69ce
SATYAJIT6253 <119771949+SATYAJIT6253@users.noreply.github.com>
2024-07-01 18:17:16 +0530

    update home section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,11 +4,11 @@ import Profile from "../assets/Profile.png";
 function Home() {
   return (
     <div className="text-white flex flex-col lg:flex-row place-content-around">
-      <div className="flex flex-col justify-center items-center gap-2 lg:w-2/5 mx-10">
-        <h1 className="text-5xl md:text-4xl">Hi There!</h1>
-        <h1 className="text-4xl"> I am <span className="text-pink-500">Satyajit Sahoo</span></h1>
-        <h1 className="text-3xl"> a passionate <span className="text-pink-600">Softawre Developer</span></h1>
-        <p className="p-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut ipsum temporibus, commodi ut suscipit nostrum. Ipsa quibusdam sunt aspernatur esse dignissimos doloribus alias, est illo nulla temporibus perspiciatis perferendis dolores modi veritatis laudantium nam sint facere similique velit. Deleniti deserunt officia .</p>
+      <div className="flex flex-col justify-center items-center gap-2 lg:w-2/5">
+        <h1 className="text-5xl flex md:text-4xl">Hi There!</h1>
+        <h1 className="text-4xl flex gap-3"> I am <span className="text-pink-500">Satyajit Sahoo</span></h1>
+        <h1 className="text-2xl flex gap-2"> a passionate <span className="text-pink-600"> Softawre Developer</span></h1>
+        <p className="p-5 flex mx-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut ipsum temporibus, commodi ut suscipit nostrum. Ipsa quibusdam sunt aspernatur esse dignissimos doloribus alias, est illo nulla temporibus perspiciatis perferendis dolores modi veritatis laudantium nam sint facere similique velit. Deleniti deserunt officia .</p>
       </div>
       <div className="flex items-center justify-center lg:w-1/3">
         <img src={Profile} alt="" />
